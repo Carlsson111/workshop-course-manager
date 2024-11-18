b48933570cd72af41f604d145732ff9d7ad070be
Carlsson111 <Linusjaa1@gmail.com>
2024-11-18 10:57:21 +0100

    Models tested and working fine

--- src/test/java/se/lexicon/course_manager/model/CourseTest.java
+++ src/test/java/se/lexicon/course_manager/model/CourseTest.java
@@ -6,19 +6,50 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CourseTest {
-     Course testObject;
-     Student student;
+     Course course;
+     Student student1;
+     Student student2;
 
     @BeforeEach
     void setUp() {
-        testObject = new Course(1, "Java Advanced", LocalDate.of(2023, 1, 1), 10);;
-        student = new Student(1, "Linus", "linus.tes@test.nu", "123 Main St");    }
+        course = new Course(1, "Java Advanced", LocalDate.of(2024, 11, 18), 10);;
+        student1 = new Student(1, "Linus", "linus.tes@test.nu", "Vintergatan 8");
+        student2 = new Student(2,"Carl", "Carl.te@test.nu", "Vintergatan 7");
+    }
+
+    @Test
+    void Constructor() {
+        Course course = new Course(1, "Java Advanced", LocalDate.of(2024, 11, 18), 10);
+        assertEquals(1, course.getId());
+        assertEquals("Java Advanced" , course.getCourseName());
+        assertEquals(LocalDate.of(2024, 11, 18), course.getStartDate());
+        assertEquals(10, course.getWeekDuration());
+
+    }
+
+
     @Test
     void enrollStudent(){
-        testObject.enrollStudent(student);
+        assertTrue(course.enrollStudent(student1));
+        assertFalse(course.enrollStudent(student1));
+        assertTrue(course.getStudents().contains(student1));
 
-        Assertions.assertTrue(testObject.getStudents().contains(student));
+    }
+    @Test
+    void unrollStudentTest(){
+        course.enrollStudent(student1);
+        assertTrue(course.unrollStudent(student1));
+        assertFalse(course.getStudents().contains(student1));
+
+
+    }
+    @Test
+    void toStringTest(){
+        String expected = "Course{id=1, courseName='Java Advanced', startDate=2024-11-18, weekDuration=10, students=[]}";
+        assertEquals(expected, course.toString());
     }
 
 
--- src/test/java/se/lexicon/course_manager/model/StudentTest.java
+++ src/test/java/se/lexicon/course_manager/model/StudentTest.java
@@ -1,5 +1,43 @@
 package se.lexicon.course_manager.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class StudentTest {
     // TODO Implement your tests here
+    private Student student;
+
+    @BeforeEach
+    public void setUp() {
+        student = new Student(1, "Linus", "linus.tes@test.nu", "Vintergatan 8");
+
+    }
+    @Test
+    public void constructorTest(){
+        Student student = new Student(1, "Linus", "linus.tes@test.nu", "Vintergatan 8");
+        assertEquals(1, student.getId());
+        assertEquals("Linus", student.getName());
+        assertEquals("linus.tes@test.nu", student.getEmail());
+        assertEquals("Vintergatan 8", student.getAddress());
+    }
+
+    @Test
+    public void EqualsAndHashCodeTest(){
+        Student student1 = new Student(1, "Linus", "linus.tes@test.nu", "Vintergatan 8");
+        Student student2 = new Student(1, "Linus", "linus.tes@test.nu", "Vintergatan 8");
+        Student student3 = new Student(2, "Carl", "Carl.tes@test.nu", "Vintergatan 7");
+        assertEquals(student1, student2);
+        assertNotEquals(student1, student3);
+        assertEquals(student1.hashCode(), student2.hashCode());
+        assertNotEquals(student1.hashCode(), student3.hashCode());
+
+    }
+    @Test
+    public void toStringTest(){
+        String expected="Student{id=1, name='Linus', email='linus.tes@test.nu', address='Vintergatan 8'}";
+        assertEquals(expected, student.toString());
+    }
+
 }
