ba64dbf7df53b64cd3f5fe977fd016a72da61248
Carlsson111 <Linusjaa1@gmail.com>
2024-11-18 12:15:03 +0100

    Daos tested and working fine

--- src/test/java/se/lexicon/course_manager/data/dao/CourseCollectionRepositoryTest.java
+++ src/test/java/se/lexicon/course_manager/data/dao/CourseCollectionRepositoryTest.java
@@ -1,17 +1,28 @@
 package se.lexicon.course_manager.data.dao;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import se.lexicon.course_manager.data.sequencers.CourseSequencer;
+import se.lexicon.course_manager.model.Course;
+import se.lexicon.course_manager.model.Student;
 
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = {CourseCollectionRepository.class})
 public class CourseCollectionRepositoryTest {
+    private CourseCollectionRepository repository;
+    private Course course1;
+    private Course course2;
+    private Student student;
 
     @Autowired
     private CourseDao testObject;
@@ -23,8 +34,69 @@ public class CourseCollectionRepositoryTest {
     }
 
     // TODO Write your tests here
+    @BeforeEach
+    void setup(){
+        Collection<Course> courses= new HashSet<>();
+        repository= new CourseCollectionRepository(courses);
+        student = new Student(1, "Linus", "linus.tes@test.nu", "Vintergatan 8");
+        course1 = new Course(1, "Java Advanced", LocalDate.of(2024, 11, 18), 10);
+        course2 = new Course(2, "Java Beginner", LocalDate.of(2024, 12, 18), 10);
+        course1.enrollStudent(student);
+        courses.add(course1);
+        courses.add(course2);
+    }
+    @Test
+    void createCourseTest(){
+        Course newCourse = repository.createCourse("Node Js", LocalDate.of(2024,1,1),10);
+        assertNotNull(newCourse);
+        assertEquals("Node Js", newCourse.getCourseName());
+        assertEquals(LocalDate.of(2024,1,1), newCourse.getStartDate());
+        assertEquals(10, newCourse.getWeekDuration());
+    }
+    @Test
+    void findByIDTest(){
+        Course found= repository.findById(1);
+        assertEquals("Java Advanced", found.getCourseName());
 
+    }
 
+    @Test
+    void findByNameContains() {
+        Collection<Course> found= repository.findByNameContains("Java");
+        assertEquals(2, found.size());
+    }
+
+    @Test
+    void findByDateBefore() {
+        Collection<Course> foundC= repository.findByDateBefore(LocalDate.of(2024,11,20));
+        assertEquals(1, foundC.size());
+    }
+
+    @Test
+    void findByDateAfter() {
+        Collection<Course> foundCourses = repository.findByDateAfter(LocalDate.of(2024,11,18));
+        assertEquals(1, foundCourses.size());
+    }
+
+    @Test
+    void findAll() {
+        Collection<Course> allCourses = repository.findAll();
+        assertEquals(2, allCourses.size());
+    }
+
+    @Test
+    void findByStudentId() {
+        Collection<Course> studentC = repository.findByStudentId(1);
+        assertEquals(1, studentC.size());
+
+    }
+
+    @Test
+    void removeCourse() {
+        boolean removed = repository.removeCourse(course1);
+        assertTrue(removed);
+        assertEquals(1, repository.findAll().size());
+    }
     @AfterEach
     void tearDown() {
         testObject.clear();
--- src/test/java/se/lexicon/course_manager/data/dao/StudentCollectionRepositoryTest.java
+++ src/test/java/se/lexicon/course_manager/data/dao/StudentCollectionRepositoryTest.java
@@ -1,17 +1,26 @@
 package se.lexicon.course_manager.data.dao;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import se.lexicon.course_manager.data.sequencers.StudentSequencer;
+import se.lexicon.course_manager.model.Student;
 
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = {StudentCollectionRepository.class})
 public class StudentCollectionRepositoryTest {
+    private StudentCollectionRepository repository;
+    private Student student1;
+    private Student student2;
+
 
     @Autowired
     private StudentDao testObject;
@@ -25,6 +34,64 @@ public class StudentCollectionRepositoryTest {
     // TODO Write your tests here
 
 
+    @BeforeEach
+    void setUp() {
+        Collection<Student> students= new HashSet<>();
+        repository = new StudentCollectionRepository(students);
+        student1= new Student(1,"Linus", "Linus.te@test.nu", "Vintergatan 8");
+        student2= new Student(2,"Carl", "Carl.te@test.nu", "Vintergatan 7");
+        students.add(student1);
+        students.add(student2);
+
+    }
+
+    @Test
+    void createStudentTest() {
+        Student newStudent= repository.createStudent("Kalle","Kalle.te@test.nu","Vintergatan 6");
+        assertEquals("Kalle", newStudent.getName());
+        assertEquals("Kalle.te@test.nu", newStudent.getEmail());
+        assertEquals("Vintergatan 6", newStudent.getAddress());
+
+    }
+
+    @Test
+    void findByEmailIgnoreCaseTest() {
+        Student foundS = repository.findByEmailIgnoreCase("Linus.te@test.nu");
+        assertEquals("Linus", foundS.getName());
+
+    }
+
+    @Test
+    void findByNameContainsTest() {
+        Collection<Student> foundStudents = repository.findByNameContains("Linus");
+        assertEquals(1, foundStudents.size());
+    }
+
+    @Test
+    void findByIdTest() {
+        Student foundS = repository.findById(1);
+        assertEquals("Linus", foundS.getName());
+    }
+
+    @Test
+    void findAllTest() {
+        Collection<Student> allS= repository.findAll();
+        assertEquals(2, allS.size());
+
+    }
+
+    @Test
+    void removeStudentTest() {
+        boolean removed = repository.removeStudent(student1);
+        assertTrue(removed);
+        assertEquals(1, repository.findAll().size());
+    }
+
+    @Test
+    void clearTest() {
+        repository.clear();
+        assertEquals(0, repository.findAll().size());
+    }
 
     @AfterEach
     void tearDown() {
